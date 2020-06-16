fa606b8d2c2bff86a66f0bdebb8cae55413d19f6
Filip Oščádal <f@mxd.cz>
2020-06-16 10:21:49 +0200

    added debugging information

--- www/upload.php
+++ www/upload.php
@@ -2,9 +2,9 @@
 
 foreach ($_FILES as $key => &$file) {
     if (!move_uploaded_file($file['tmp_name'], "./upload/" . basename($file['name']))) {
-        echo "Failed to save uploaded file '" . $file['name'];
+        echo "Failed to save uploaded file '" . $file['name'] . "\n";
     }
 }
 
 // debugging information
-print_r( $_FILES );
+print_r($_FILES);
